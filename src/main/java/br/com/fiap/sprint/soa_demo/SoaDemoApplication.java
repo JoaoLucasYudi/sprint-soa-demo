5c5ce4807bccdd53a196cb9db89218dc2b9bcbe9
JoaoLucasYudi <joaolucasyudi@gmail.com>
2025-06-13 11:43:41 -0300

    arquivo pom configurado e projeto iniciado

---
+++ src/main/java/br/com/fiap/sprint/soa_demo/SoaDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprint.soa_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/sprint/soa_demo/SoaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprint.soa_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
